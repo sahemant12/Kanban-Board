70f087e8504afbe735e5e64c0dc2ed496b0ab42d
Hemant Sah <hemantsah2912@gmail.com>
2025-03-14 03:14:32 +0530

    update modify-board

--- add.board.js
+++ add.board.js
@@ -1,54 +1,61 @@
 import boardObject from "./data.js"
-import {renderBoard, addTask, reRender, dragNdrop, dragOverBoard} from "./script.js"
+import {renderBoard, addTask, reRender, dragOverBoard, getCurrBoardObj} from "./script.js"
 
 const addBoardBtn = document.getElementById("add-board-btn");
 const boardInfo = document.getElementById("board-info");
-const crossBtn = document.getElementById("cross");
+const crossBtn = document.getElementById("cross"); 
 const createBoardBtn = document.getElementById("create-btn");
 const container = document.getElementById("container");
 const selectColor = document.querySelectorAll(".circle-color");
+const boardCreateDiv = document.getElementById("board-create-btn");
+const boardEditDiv = document.getElementById("board-edit-btn");
+const editCancelBtn = document.getElementById("cancel-btn");
+const editSubmitBtn = document.getElementById("sumbit-btn");
+const modifyBoardIcon = document.querySelectorAll(".modify-board-icon");
 
-addBoardBtn.addEventListener("click",()=>{
+let selectedColor = "#1a5ae4";
+let getCurrBoardObject = null;
+
+//add new board Button
+addBoardBtn.addEventListener("click", addBoard);
+
+crossBtn.addEventListener("click", hideBoardInfoContainer);
+
+selectColor.forEach((color)=>{
+    color.addEventListener("click", () => selectBoardHeadColor(color));
+});
+
+createBoardBtn.addEventListener("click", createBoard);
+
+function addBoard(){
+    crossBtn.classList.remove("hidden");
+    boardCreateDiv.classList.remove("hidden");
+    boardEditDiv.classList.add("hidden");
+    displayBoardInfoContainer();
+}
+
+function displayBoardInfoContainer(){
     boardInfo.style.display = "block";
     container.style.opacity = "0.7";
     container.style.pointerEvents = "none";
+}
 
-    // const newBoard ={   
-    //     id:Date.now(),
-    //     boardName: "Hello",
-    //     description: "Am I working",
-    //     tasks: [],
-    //     color:selectedColor
-    // }
-    // boardObject.push(newBoard);
-
-    // const currentBoard = renderBoard(newBoard);       
-    // addTask(currentBoard.children[2].children[0]);
-    // reRender();
-    
-})
-
-crossBtn.addEventListener("click",()=>{
+function hideBoardInfoContainer(){
     boardInfo.style.display = "none";
     container.style.opacity = "1";
     container.style.pointerEvents = "auto"; 
-})
+}
 
-
-let selectedColor = "#1a5ae4";
-selectColor.forEach((color)=>{
-    color.addEventListener("click",()=>{
-        for(let removeColor of selectColor){
-            if(color == removeColor){
-                continue;               
-            }
-            removeColor.classList.remove("selected-color");
+function selectBoardHeadColor(color){
+    for(let removeColor of selectColor){
+        if(color == removeColor){
+            continue;               
         }
-        color.classList.toggle("selected-color");
-        selectedColor = rgbToHex(window.getComputedStyle(color).backgroundColor);
-            
-    })
-})
+        removeColor.classList.remove("selected-color");
+    }
+    color.classList.toggle("selected-color");
+    selectedColor = rgbToHex(window.getComputedStyle(color).backgroundColor);
+}
 
 function rgbToHex(rgb) {
     const [r, g, b] = rgb.match(/\d+/g);
@@ -56,8 +63,7 @@ function rgbToHex(rgb) {
 }
 
 
-createBoardBtn.addEventListener("click",()=>{
-    
+function createBoard(){
     const boardName = document.getElementById("board-name");
     const boardDescription = document.getElementById("board-description");
     const boardNameValue = boardName.value;
@@ -72,66 +78,91 @@ createBoardBtn.addEventListener("click",()=>{
         tasks: [],
         color:selectedColor
     }
-    // const newBoard ={   
-    //     id:Date.now(),
-    //     boardName: "Hello",
-    //     description: "Am I working",
-    //     tasks: [],
-    //     color:selectedColor
-    // }
     boardObject.push(newBoard);
 
     const currentBoard = renderBoard(newBoard);       
-    addTask(currentBoard.children[2].children[0]);
+    addTask(currentBoard.querySelector(".add-task-btn"));
     reRender();
-    // dragNdrop();
     dragOverBoard(currentBoard);
     boardName.value="";
     boardDescription.value="";
+    hideBoardInfoContainer();
 
-    boardInfo.style.display = "none";
-    container.style.opacity = "1";
-    container.style.pointerEvents = "auto";
-
-})
+    currentBoard.querySelector(".modify-board-icon").addEventListener("click", boardInfoModify);
+}
 
 
-// delete-board
-const modifyBoardIcon = document.querySelectorAll(".modify-board-icon");
 modifyBoardIcon.forEach((modifyboard)=>{
-    modifyboard.addEventListener("click",(e)=>{
-        const displayModifyContainer = e.target.parentElement?.children[4];
-        if(displayModifyContainer.classList.contains("modify-board-container")){
-            displayModifyContainer.classList.remove("hidden-modify-board");           
-        }
+    modifyboard.addEventListener("click", boardInfoModify);
+});
 
-        const deleteBoardBtn = displayModifyContainer.children[1];
-        const editBoardBtn = displayModifyContainer.children[0];
-        const mainBoard = displayModifyContainer.parentElement.parentElement.parentElement;
-        const boardId = mainBoard.dataset.id;
-        let modifyBoardIndex = -1;
-        boardObject.forEach((ele, index)=>{
-            if(ele.id == boardId){
-                modifyBoardIndex = index;
-            }
-        })
-        // let hope = boardObject.indexOf(boardId);
-        console.log(modifyBoardIndex);
-        
-        deleteBoardBtn.addEventListener("click",()=>{
-            // mainBoard.remove();
-            
-            boardObject = boardObject.filter((id)=>boardId!=id);
-            console.log(boardObject);
-            
-            
-        })
-        editBoardBtn.addEventListener("click",()=>{
-            console.log("edit");
-            
-        })
+function boardInfoModify(e){
+    const allModifyContainer = document.getElementsByClassName("modify-board-container");
+    
+    const displayModifyContainer = e.target.nextElementSibling;
+    for(let remove of allModifyContainer){
+        if(displayModifyContainer == remove) continue;        
+        remove.classList.add("hidden-modify-board");
+    }
+    if(displayModifyContainer.classList.contains("modify-board-container")){
+        displayModifyContainer.classList.toggle("hidden-modify-board");           
+    }
+    
+    //get delete and edit button
+    const deleteBoardBtn = displayModifyContainer.children[1];
+    const editBoardBtn = displayModifyContainer.children[0];
 
-        
-        
+    //get the board on which we perform operation.
+    const mainBoard = displayModifyContainer.closest(".board");
+    const mainBoardId = mainBoard.dataset.id;
+
+    //get the index of board on which we perform operation
+    const modifyBoardIndex = boardObject.findIndex((obj)=> mainBoardId==obj.id);
+
+    //get current board object
+    getCurrBoardObject = getCurrBoardObj(mainBoard);
+
+    //delete the select board
+    deleteBoardBtn.addEventListener("click",()=>{           
+        boardObject.splice(modifyBoardIndex, 1);
+        mainBoard.remove();               
     })
-})
+
+    const boardName = document.getElementById("board-name");
+    const boardDescription = document.getElementById("board-description");
+
+    editBoardBtn.addEventListener("click",()=>{
+        if(!getCurrBoardObject) return;
+
+        crossBtn.classList.add("hidden");
+        boardCreateDiv.classList.add("hidden");
+        boardEditDiv.classList.remove("hidden");
+
+        displayModifyContainer.classList.add("hidden-modify-board");
+        displayBoardInfoContainer();
+
+        boardName.value = getCurrBoardObject.boardName;
+        boardDescription.value = getCurrBoardObject.description;
+        editCancelBtn.addEventListener("click", hideBoardInfoContainer);        
+    })
+}
+editSubmitBtn.addEventListener("click", submitEdited);
+
+function submitEdited(){
+    const boardName = document.getElementById("board-name");
+    const boardDescription = document.getElementById("board-description");
+    if(!getCurrBoardObject) return;
+    getCurrBoardObject.boardName = boardName.value;
+    getCurrBoardObject.description = boardDescription.value;
+    getCurrBoardObject.color = selectedColor;
+
+    const mainBoardly = document.querySelector(`.board[data-id="${getCurrBoardObject.id}"]`);
+    if(mainBoardly){
+        mainBoardly.querySelector("h4").textContent = getCurrBoardObject.boardName;
+        mainBoardly.querySelector("#description").textContent = getCurrBoardObject.description;
+        mainBoardly.querySelector("#circle-icon").style.borderColor = getCurrBoardObject.color;
+    }
+    boardName.value="";
+    boardDescription.value="";
+    hideBoardInfoContainer();    
+}
--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 import boardObject from "./data.js"
 const container = document.getElementById("container");
-
-//render all board present in database//1
-boardObject.forEach((board)=>renderBoard(board));
-
-// boardObject.forEach((board)=>{
-//     console.log("main: ", board.tasks);
-// });
-
 let allBoards;
 let allTasks;
 let addTaskBtn;
+
+//1. render all board present in local storage
+boardObject.forEach((board)=>renderBoard(board));
+
+reRender();
+dragNdrop();
+
+//2.
+addTaskBtn.forEach(addTask);
+
+
+
 function reRender() {
     allBoards = document.querySelectorAll(".board");
     allTasks = document.querySelectorAll(".task");
     addTaskBtn = document.querySelectorAll(".add-task-btn");
 }
-{/* <button id="delete-board-btn" data-id=${board.id} class="delete-board-btn">X</button> */}
-reRender();
-dragNdrop();
-//render each board
+
+
+
 function renderBoard(board){
 
     //create div element that have board  
@@ -46,27 +49,22 @@ function renderBoard(board){
             </div>`
     container.appendChild(divBoard);
 
-    //taskList will have all the task of given board
+    //2. taskList will have all the task of given board
     const taskList = document.getElementById(`${board.id}`);
-
-    // const divList = document.createElement("div");
-    // divList.setAttribute("id",`board-${board.id}`);
-    // divList.setAttribute("class","task-list");
-    
-    //2
     board.tasks.forEach((task)=>taskList.appendChild(renderTask(task)));
-    // taskList.appendChild(divList);
+
     return divBoard;
 }
 
 //render each task
 function renderTask(task){
+
     const liTask = document.createElement("li");
     liTask.setAttribute("class", "task");
     liTask.setAttribute("draggable", "true"); 
     liTask.innerHTML = `<div class="task-name">
                         <i class="fa-solid fa-circle-check" style="color: #afb1af; text-dec"></i>
-                        <span>${task}</span>
+                        <span class="task-value">${task}</span>
                     </div>
                     <div class="task-modify">
                         <i class="fa-solid fa-trash delete-icon" style="color: #616161;"></i>
@@ -76,13 +74,7 @@ function renderTask(task){
 }
 
 
-
-//3
-addTaskBtn.forEach((taskBtn)=>{
-    addTask(taskBtn);
-})
 function addTask(target){
-
     target.addEventListener("click",()=>{
 
         //give prompt to create new task
@@ -106,57 +98,50 @@ function addTask(target){
         updateTotalTask();
         
         //drag this current-Task
-        dragTask(newTask);
-        dragOverBoard(target.parentElement?.parentElement);
-        //delete-edit task
+        dragTask(newTask);       
+        dragOverBoard(target.closest(".board"));
+
+        //modifyTask new task
         modifyTask(newTask); 
         
-        // boardObject.forEach((board)=>{
-        //     console.log("add-task: ", board.tasks);
-        // });
     })
 }
-
-
-//4
-// const allBoards = document.querySelectorAll(".board");
-// const allTasks = document.querySelectorAll(".task");
-// const addTaskBtn = document.querySelectorAll(".add-task-btn");
 
 function dragNdrop(){
 
     allTasks.forEach((task)=>{
         dragTask(task);
         
-        //delete-edit-task
+        //modifyTask
         modifyTask(task);  
     })
     
     allBoards.forEach((board)=>{
-        // console.log(board);
         dragOverBoard(board);
     })
 }
 
-function dragOverBoard(board){
-    
-    board.addEventListener("dragover",(e)=>{
-        e.preventDefault();
-        
-        const movingTask = document.querySelector(".flying");
-        if (!movingTask) return;
-        
-        //append this movingTask to current board
-        const allTaskElement = board.children[1];    
-        allTaskElement.appendChild(movingTask);
-
-    })
+function dragOverBoard(board){  
+    board.addEventListener("dragover", () => dragOver(board));
 }
-function dragTask(target){
-    
-    target.addEventListener("dragstart",()=>{
-        target.classList.add("flying");
 
+function dragOver(board){
+      
+    const movingTask = document.querySelector(".flying");
+    if (!movingTask) return;
+    
+    //append this movingTask to current board    
+    const allTaskElement = board.querySelector(".tasks-list");    
+    allTaskElement.appendChild(movingTask);
+}
+
+function dragTask(target){  
+    target.addEventListener("dragstart", () => dragStart(target));
+    target.addEventListener("dragend", () => dragEnd(target));
+}
+
+function dragStart(target){
+    target.classList.add("flying");
         //update board Object
         const targetBoard = target.parentElement.parentElement;
         const currBoardObj = getCurrBoardObj(targetBoard);
@@ -165,11 +150,10 @@ function dragTask(target){
         const removeTaskIndex = currBoardObj.tasks.indexOf(movingTaskValue);
         currBoardObj.tasks.splice(removeTaskIndex, 1);
         updateTotalTask();
-    })
-    target.addEventListener("dragend",()=>{
+}
 
-        target.classList.remove("flying");
-        
+function dragEnd(target){
+    target.classList.remove("flying");       
         //update board Object
         const targetBoard = target.parentElement.parentElement;
         const currBoardObj = getCurrBoardObj(targetBoard);
@@ -177,10 +161,6 @@ function dragTask(target){
               
         currBoardObj.tasks.push(movingTaskValue);
         updateTotalTask();
-        // boardObject.forEach((board)=>{
-        //     console.log("dragEnd: ", board.tasks);
-        // });
-    })
 }
 
 //update allTaskList
@@ -204,60 +184,27 @@ function modifyTask(taskModify){
     taskModify.addEventListener("click",(e)=>{
         
         if(e.target.classList.contains("delete-icon") || e.target.classList.contains("edit-icon")){            
-            const targetBoard = e.target.parentElement?.parentElement?.parentElement?.parentElement;            
+            // const targetBoard = e.target.parentElement?.parentElement?.parentElement?.parentElement;            
+            const targetBoard = e.target.closest(".board");
+
             const currBoardObj = getCurrBoardObj(targetBoard);             
-            const TaskValue = e.target.parentElement.parentElement.querySelector("span").textContent;             
+            // const TaskValue = e.target.parentElement.parentElement.querySelector("span").textContent;             
+            const TaskValue = e.target.closest(".task").querySelector(".task-value").textContent;
+                         
             const removeTaskIndex = currBoardObj.tasks.indexOf(TaskValue);
 
             if(e.target.classList.contains("delete-icon")){
                 currBoardObj.tasks.splice(removeTaskIndex, 1);
                 updateTotalTask();
                 taskModify.remove();
-                // boardObject.forEach((board)=>{
-                //     console.log("delete: ", board.tasks);
-                // });
             }else{
                 const editedTodo = prompt("Enter your task:", TaskValue).trim();
                 if(!editedTodo) return;   
                 currBoardObj.tasks.splice(removeTaskIndex, 1, editedTodo);
-                e.target.parentElement.parentElement.querySelector("span").textContent = editedTodo;
-                // boardObject.forEach((board)=>{
-                //     console.log("edit: ", board.tasks);
-                // });
-            }
-            
-            
+                e.target.closest(".task").querySelector(".task-value").textContent = editedTodo;
+            }            
         }
     })
 }
 
-// const taskName = document.querySelectorAll(".task-name");
-
-// taskName.forEach((taskspan)=>{
-//     completed(taskspan);  
-// })
-
-// function completed(target){
-//     target.addEventListener("click",()=>{
-//         target.children[1].classList.toggle("add-strike");
-//         target.children[0].classList.toggle("completed");
-//     })
-// }
-
-
-
-// function getDragAfterElement(container, y) {
-//     const draggableElements = [...container.querySelectorAll('.draggable:not(.flying)')]
-  
-//     return draggableElements.reduce((closest, child) => {
-//       const box = child.getBoundingClientRect();
-//       const offset = y - box.top - box.height / 2
-//       if (offset < 0 && offset > closest.offset) {
-//         return { offset: offset, element: child }
-//       } else {
-//         return closest;
-//       }
-//     }, { offset: Number.NEGATIVE_INFINITY }).element
-//   }
-
-export {renderBoard, addTask, reRender, dragNdrop, dragOverBoard};
+export {renderBoard, addTask, reRender, dragOverBoard, getCurrBoardObj};
